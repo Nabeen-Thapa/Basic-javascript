f4d97260eadbbaa44d4a523bd0906d05f031ba01
Nabin Thapa <151662784+Nabeen-Thapa@users.noreply.github.com>
2024-09-07 23:56:35 +0545

    day 1 of js

---
+++ comment.js
@@ -0,0 +1,4 @@
+// comment of js
+// comments are ignored by language processor
+console.log("this is comment page");
+// console.log("hello world, thos is my comment");
---
+++ execution_flow.js
@@ -0,0 +1,8 @@
+// node.js follow top to down approach to exucute
+console.log("hey, i am Nabin");
+console.log("and i m learning exexution flow");
+console.log("in javascript");
+console.log(1);
+console.log(2);
+console.log(3);
+console.log("start");
