708fea217c9127639ea35af16382e2d442c850e5
Nabin Thapa <151662784+Nabeen-Thapa@users.noreply.github.com>
2024-09-09 01:27:52 +0545

    day 2 of js

---
+++ day2_exercise3.js
@@ -0,0 +1,7 @@
+// guess the result
+console.log(19 % 3);                    //1
+console.log(10 == 3);                   //flase
+console.log(10 !== "10");               //true
+console.log(2 < "10");                  //true           
+console.log("5" > 2);                   //true
+console.log((false && true) || false);  //false
---
+++ day2_operator.js
@@ -0,0 +1,64 @@
+// Arithematic oprators
+//      - total 8 Arithematic oprators -- +, -, /, *, %, ++, --, **
+let x= "30";
+let y= 20;
+x= Number(x);
+console.log("sum is :" + (x+y));
+console.log("sub is :" + (x-y));
+console.log("mul is :" + (x*y));
+console.log("mul is :" + (x**y));
+console.log("div is :" + (x/y));
+console.log("mod is :" + (x%y));
+console.log("inc is :" + (x++));
+console.log("dec is :" + (y--));
+
+
+console.log("hi " +80);     //string and number
+
+
+
+// The assignment operator - = is used to asign any value to hte variable
+let msg = "hlo";    //hlo is assign to msg variable
+let on = true;
+console.log(x=y);
+console.log(x+=y);      //x=x+y
+console.log(x-=y);      //x=x-y
+console.log(x*=y);      //x=x*y;
+console.log(x/=y);      //x=x/y;
+console.log(x%=y);      //x=x%y;
+
+
+
+
+// The comparison operators - return boolean value : true, false
+console.log(x==y);      //return false
+console.log(x!=y);      //return true
+console.log(x!==y);      //return true
+console.log(x>y);      //return false
+console.log(x<y);      //return true
+console.log(x>=y);      //return false
+console.log(x<=y);      //return true
+console.log(x!=y);      //return true
+console.log("ABC" == "ABC");      //return true
+console.log("ABC" == "Aabc");      //return false - case sensitive so it return false
+console.log(10 == "10");      //return true 
+
+
+
+//logical operator
+console.log(4> 50 && 8<30); //retunr false   (fasle && true = false)
+console.log(4> 50 || 8<30); //retunr true   (fasle || true = false)
+console.log(!true);
+
+
+// type of oerator
+console.log(typeof (x));
+console.log(typeof (x));
+console.log(typeof"type of operator");
+
+
+
+
+
+
+
